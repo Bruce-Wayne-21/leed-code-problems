7fca14e8b3fbd732224a30b1444c6e897988ec9d
Saji.P <sajilan@unicornconnectedapps.com>
2026-03-12 20:59:39 +0530

    create the sample app

--- oop/oop/Program.cs
+++ oop/oop/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// OOP Concepts Demo
+
+var dog = new Dog("Buddy", 3);
+var cat = new Cat("Whiskers", 5);
+
+Animal[] animals = [dog, cat];
+
+foreach (var animal in animals)
+{
+    Console.WriteLine(animal);
+    animal.Speak();
+    Console.WriteLine();
+}
+
+// Abstraction & Encapsulation
+public abstract class Animal(string name, int age)
+{
+    public string Name { get; } = name;
+    public int Age { get; } = age;
+
+    public abstract void Speak();
+
+    public override string ToString() => $"{GetType().Name}: {Name}, Age: {Age}";
+}
+
+// Inheritance & Polymorphism
+public class Dog(string name, int age) : Animal(name, age)
+{
+    public override void Speak() => Console.WriteLine($"{Name} says: Woof!");
+}
+
+public class Cat(string name, int age) : Animal(name, age)
+{
+    public override void Speak() => Console.WriteLine($"{Name} says: Meow!");
+}
