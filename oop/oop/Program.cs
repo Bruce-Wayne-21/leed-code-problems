965cf565dcf8e811f9683fa34935a16695c25c60
Saji.P <ut01261tic2024@gmail.com>
2026-07-05 16:22:20 +0530

    practice while loop and do while loop

--- oop/oop/Program.cs
+++ oop/oop/Program.cs
@@ -25,10 +25,19 @@ public class Program
         //leedcode.problms problms = new leedcode.problms();
         //await problms.PrintOneAsync();
 
-        leedcode.problms.solution_2 solution_2 = new leedcode.problms.solution_2();
-        var value = solution_2.ConvertToTitle(82595524);
+        //leedcode.problms.solution_2 solution_2 = new leedcode.problms.solution_2();
+        //var value = solution_2.ConvertToTitle(82595524
+        //
 
-        Console.WriteLine(value);
+        //DSA.Datastructure.printnum();
+        //DSA.Datastructure.PrintNumber();
+
+        DSA.Datastructure.dowhile();
+
+
+
+
+        //Console.WriteLine(value);
 
         //Console.WriteLine(value);
 
---
+++ oop/oop/class/DSA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop
+{
+    internal class DSA
+    {
+        public class Datastructure
+        {
+            public static void PrintNumber()
+            {
+                int number = 0;
+                int i = 0;
+                string j = Console.ReadLine();
+                int.TryParse(j, out i);
+
+                while (number > i)
+                {
+                    Console.WriteLine(number);
+                     number ++;
+                }
+
+            }
+
+
+            public static void printnum()
+            {
+                int num = 0;
+                while(num < 5)
+                {
+                    Console.WriteLine(num);
+                    num++;
+
+                }
+            }
+
+
+            public static void dowhile()
+            {
+                int count = 10;
+                do
+                {
+                    Console.WriteLine($"This is a do-while loop.{count}");
+                    count++;
+
+                }
+                while (count < 3);
+            }
+        }
+    }
+}
