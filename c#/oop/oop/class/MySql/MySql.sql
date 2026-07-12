097634a5fb91f499cddc412d53b9075ee3a2d08d
Saji.P <ut01261tic2024@gmail.com>
2026-07-12 21:57:02 +0530

    start my sql problem solving

---
+++ c#/oop/oop/class/MySql/MySql.sql
@@ -0,0 +1,18 @@
+﻿--Managers with at Least 5 Direct Reports--
+WITH cte AS (
+    SELECT 
+        COUNT(*) AS ManageridCount,
+        e.managerId
+    FROM Employee AS e
+    WHERE e.managerId IS NOT NULL 
+    GROUP BY e.managerId 
+    HAVING COUNT(*) >= 5
+)
+
+-- select * from cte where
+-- select name from employee where id = 101
+
+select
+    ed.name as [name]
+from employee as ed
+inner join cte as c on ed.id = c.managerid
